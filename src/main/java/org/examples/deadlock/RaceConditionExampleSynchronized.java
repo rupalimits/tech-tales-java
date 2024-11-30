730e89ec53a5a50e77eb8d95e3688f1f8fcfa208
rupalimits <rupalipatelkvc@gmail.com>
2024-11-30 18:28:43 -0500

    adding the code of synchronization.

--- src/main/java/org/examples/deadlock/RaceConditionExampleSynchronized.java
+++ src/main/java/org/examples/deadlock/RaceConditionExampleSynchronized.java
@@ -1,4 +1,33 @@
 package org.examples.deadlock;
 
 public class RaceConditionExampleSynchronized {
+    private int counter = 0;
+
+    public static void main(String[] args) {
+        RaceConditionExampleSynchronized example = new RaceConditionExampleSynchronized();
+        example.runTest();
+    }
+
+    public void runTest() {
+        Thread t1 = new Thread(this::increment);
+        Thread t2 = new Thread(this::increment);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Counter Value: " + counter);
+    }
+
+    public synchronized void increment() {
+        for (int i = 0; i < 10000; i++) {
+            counter++;  // This is not thread-safe, leading to a race condition
+        }
+    }
 }
